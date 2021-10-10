16088a00634375a305acb3c7dbfb3ebfcaec7c8b
Tristan Read <60425965+kOFReadie@users.noreply.github.com>
2021-10-10 14:00:39 +0100

    Create tplink-lightbulb.ts
    Convert (and modify) from JS to TS

---
+++ src/tplink-lightbulb.ts
@@ -0,0 +1,599 @@
+import dgram = require('dgram');
+import EventEmitter = require('events');
+
+const OldFormat_APName = "smartlife.iot.common.softaponboarding";
+const OldFormat_LightingServiceName = "smartlife.iot.smartbulb.lightingservice";
+const OldFormat_SystemName = "smartlife.iot.common.system";
+const OldFormat_ScheduleName = "smartlife.iot.common.schedule";
+const OldFormat_CloudName = "smartlife.iot.common.cloud";
+
+function _extends(target: any, ...args: any[])
+{
+    for (var i = 1; i < args.length; i++)
+    {
+        var source = args[i];
+        for (var key in source)
+        {
+            if (Object.prototype.hasOwnProperty.call(source, key))
+            {
+                target[key] = source[key];
+            }
+        }
+    }
+    return target;
+}
+
+export class TPLSmartDevice
+{
+    private ip: string;
+    private port: number;
+    private _info?: dgram.RemoteInfo;
+    private _sysInfo?: any;
+    private host?: string;
+    private name?: any;
+    private deviceID?: any;
+
+    constructor (
+        ip: string,
+        port: number,
+        _info?: dgram.RemoteInfo,
+        _sysInfo?: any,
+        host?: string,
+        name?: any,
+        deviceID?: any
+    )
+    {
+        this.ip = ip;
+        this.port = port;
+        this._info = _info;
+        this._sysInfo = _sysInfo;
+        this.host = host;
+        this.name = name;
+        this.deviceID = deviceID;
+    }
+
+    public static Scan(filter?: string, broadcast = "255.255.255.255"): { emitter: EventEmitter, client: dgram.Socket }
+    {
+        const emitter = new EventEmitter();
+        const client = dgram.createSocket({ type: "udp4", reuseAddr: true });
+
+        client.bind(9998, undefined, () =>
+        {
+            client.setBroadcast(true);
+            const msgBuf = TPLSmartDevice.Encrypt(Buffer.from('{"system":{"get_sysinfo":{}}}'));
+            client.send(msgBuf, 0, msgBuf.length, 9999, broadcast);
+        });
+
+        client.on("message", (msg, rinfo) =>
+        {
+            const decryptedMsg = TPLSmartDevice.Decrypt(msg).toString("ascii");
+            const jsonMsg = JSON.parse(decryptedMsg);
+            const sysInfo = jsonMsg.system.get_sysinfo;
+            if (filter && sysInfo.mic_type !== filter) { return; }
+
+            const light = new TPLSmartDevice(
+                rinfo.address,
+                rinfo.port,
+                rinfo,
+                sysInfo,
+                rinfo.address,
+                sysInfo.alias,
+                sysInfo.deviceId
+            );
+            emitter.emit("light", light);
+        });
+
+        return { emitter: emitter, client: client };
+    }
+
+    public Send<T>(msg: object): Promise<T>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            if (!this.ip) { return reject(new Error("IP not set.")); }
+            if (!this.port) { return reject(new Error("Port not set.")); }
+
+            const client = dgram.createSocket("udp4");
+            const message = TPLSmartDevice.Encrypt(Buffer.from(JSON.stringify(msg)));
+
+            client.send(message, 0, message.length, this.port, this.ip, (err, bytes) =>
+            {
+                if (err) { return reject(err); }
+
+                client.on("message", clientMsg =>
+                {
+                    resolve(JSON.parse(TPLSmartDevice.Decrypt(clientMsg).toString()));
+                    client.close();
+                });
+            });
+        });
+    }
+
+    public async ListWiFi(): Promise<object>
+    {
+        const newFormatData: INewFormat =
+        {
+            netif:
+            {
+                get_scaninfo:
+                {
+                    refresh: 1
+                }
+            }
+        }
+        const r1 = await this.Send<INewFormat>(newFormatData)
+            .catch(err => { throw err; });
+        if (r1 != null && r1.netif != null && r1.netif.get_scaninfo != null && r1.netif.get_scaninfo.ap_list != null)
+        {
+            return r1.netif.get_scaninfo.ap_list;
+        }
+
+        //On fail, try the older message-format
+        const oldFormatData: IOldFormat =
+        {
+            [OldFormat_APName]:
+            {
+                get_scaninfo:
+                {
+                    refresh: 1
+                }
+            }
+        }
+        const r2 = await this.Send<IOldFormat>(oldFormatData)
+            .catch(err => { throw err; });
+        if (r2 != null && r2[OldFormat_APName] != null && r2[OldFormat_APName]!.get_scaninfo != null && r2[OldFormat_APName]!.get_scaninfo!.ap_list != null)
+        {
+            return r2[OldFormat_APName]!.get_scaninfo!.ap_list!;
+        }
+
+        return [];
+    }
+
+    public async ConnectWiFi(ssid: string, password: string, keyType = 1, cypherType = 0): Promise<boolean>
+    {
+        const newFormatData: INewFormat =
+        {
+            netif:
+            {
+                set_stainfo:
+                {
+                    ssid,
+                    password,
+                    key_type: keyType,
+                    cypher_type: cypherType
+                }
+            }
+        }
+        const r1 = await this.Send<INewFormat>(newFormatData)
+            .catch(err => { throw err; });
+        if ((
+            r1 == null ? 0 :
+            r1.netif == null ? 0 :
+            r1.netif!.set_stainfo == null ? 0 :
+            r1.netif!.set_stainfo!.err_code
+        ) === 0)
+        {
+            return true;
+        }
+
+        //On fail, try the older message-format
+        const oldFormatData: IOldFormat =
+        {
+            [OldFormat_APName]:
+            {
+                set_stainfo:
+                {
+                    ssid,
+                    password,
+                    key_type: keyType,
+                    cypher_type: cypherType
+                }
+            }
+        }
+        const r2 = await this.Send<IOldFormat>(oldFormatData)
+            .catch(err => { throw err; });
+        if ((
+            r2 == null ? 0 :
+            r2[OldFormat_APName] == null ? 0 :
+            r2[OldFormat_APName]!.set_stainfo == null ? 0 :
+            r2[OldFormat_APName]!.set_stainfo!.err_code
+        ) === 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Info(): Promise<IGetSYSInfo | null>
+    {
+        const data: INewFormat = { system: { get_sysinfo: {} } };
+        const r = await this.Send<INewFormat>(data)
+            .catch(err => { throw err; });
+        if (r != null && r.system != null && r.system.get_sysinfo != null)
+        {
+            return r.system.get_sysinfo;
+        }
+        return null;
+    }
+
+    public async Power(powerState: boolean, transition: number = 0, options: IPowerOptions = {}): Promise<IInfoResponse | null>
+    {
+        const info = await this.Info()
+            .catch(err => { throw err; });
+        if (info?.relay_state != undefined)
+        {
+            const data: INewFormat =
+            {
+                system:
+                {
+                    set_relay_state:
+                    {
+                        state: powerState ? 1 : 0,
+                    }
+                }
+            };
+            return this.Send<IInfoResponse>(data)
+                .catch(err => { throw err; });
+        }
+        else
+        {
+            const data: IOldFormat =
+            {
+                [OldFormat_LightingServiceName]:
+                {
+                    transition_light_state: _extends({
+                        ignore_default: 1,
+                        on_off: powerState ? 1 : 0,
+                        transition_period: transition
+                    }, options)
+                }
+            };
+            const r = await this.Send<IOldFormat>(data)
+                .catch(err => { throw err; });
+            if (r != null && r[OldFormat_LightingServiceName] != null && r[OldFormat_LightingServiceName]!.transition_light_state != null)
+            {
+                //TODO: Find a way to check the response data for the old format.
+                return r[OldFormat_LightingServiceName]!.transition_light_state;
+            }
+        }
+        return null;
+    }
+
+    public Led(ledState: boolean): Promise<object>
+    {
+        const data: INewFormat =
+        {
+            system:
+            {
+                set_led_off:
+                {
+                    off: ledState ? 1 : 0
+                }
+            }
+        };
+        return this.Send<object>(data)
+            .catch(err => { throw err; });
+    }
+
+    public async Name(newAlias: string)
+    {
+        const info = await this.Info()
+            .catch(err => { throw err; });
+        
+        if (info == null || info.dev_name == undefined)
+        {
+            const data: INewFormat =
+            {
+                system:
+                {
+                    set_dev_alias:
+                    {
+                        alias: newAlias
+                    }
+                }
+            };
+
+            return this.Send<object>(data)
+                .catch(err => { throw err; });
+        }
+        else
+        {
+            const data: IOldFormat =
+            {
+                [OldFormat_SystemName]:
+                {
+                    set_dev_alias:
+                    {
+                        alias: newAlias
+                    }
+                }
+            };
+
+            return this.Send<object>(data)
+                .catch(err => { throw err; });
+        }
+    }
+
+    public async DayStat(month: number, year: number): Promise<object | null>
+    {
+        const now = new Date();
+        month = month || now.getMonth() + 1;
+        year = year || now.getFullYear();
+        const data: IOldFormat =
+        {
+            [OldFormat_ScheduleName]:
+            {
+                get_daystat:
+                {
+                    month,
+                    year
+                }
+            }
+        };
+        const r = await this.Send<IOldFormat>(data)
+            .catch(err => { throw err; });
+        if (r != null && r[OldFormat_ScheduleName] != null && r[OldFormat_ScheduleName]!.get_daystat != null)
+        {
+            return r[OldFormat_ScheduleName]!.get_daystat!;
+        }
+        return null;
+    }
+
+    public async Cloud(): Promise<object | null>
+    {
+        const data: IOldFormat =
+        {
+            [OldFormat_CloudName]:
+            {
+                get_info: {}
+            }
+        };
+        const r = await this.Send<IOldFormat>(data)
+            .catch(err => { throw err; });
+        if (r != null && r[OldFormat_CloudName] != null && r[OldFormat_CloudName]!.get_info != null)
+        {
+            return r[OldFormat_CloudName]!.get_info;
+        }
+        return null;
+    }
+
+    public async Schedule()
+    {
+        const data: IOldFormat =
+        {
+            [OldFormat_ScheduleName]:
+            {
+                get_rules: {}
+            }
+        };
+        const r = await this.Send<IOldFormat>(data)
+            .catch(err => { throw err; });
+        if (r != null && r[OldFormat_ScheduleName] != null && r[OldFormat_ScheduleName]!.get_rules != null)
+        {
+            return r[OldFormat_ScheduleName]!.get_rules;
+        }
+        return null;
+    }
+
+    public Details(): Promise<any>
+    {
+        const data: IOldFormat =
+        {
+            [OldFormat_LightingServiceName]:
+            {
+                get_light_details: {}
+            }
+        };
+        return this.Send<IOldFormat>(data)
+            .catch(err => { throw err; });
+    }
+
+    public Reboot()
+    {
+        const data: IOldFormat =
+        {
+            [OldFormat_SystemName]:
+            {
+                reboot:
+                {
+                    delay: 1
+                }
+            }
+        };
+        return this.Send<IOldFormat>(data)
+            .catch(err => { throw err; });
+    }
+
+    public static Encrypt(buf: Buffer, key = 0xAB): Buffer
+    {
+        for (let i = 0; i < buf.length; i++)
+        {
+            const char = buf[i];
+            buf[i] = char ^ key;
+            key = buf[i];
+        }
+        return buf;
+    }
+
+    public static Decrypt(buf: Buffer, key = 0xAB): Buffer
+    {
+        for (let i = 0; i < buf.length; i++)
+        {
+            const char = buf[i];
+            buf[i] = char ^ key;
+            key = char;
+        }
+        return buf;
+    }
+}
+
+//#region Interfaces
+//I need a full response with every data type and object avaliable so I know what types to put below and not have all of them nullable.
+//#region Networking
+interface IGetScanInfo
+{
+    refresh?: number,
+    ap_list?: object
+}
+
+interface ISetSTAInfo
+{
+    ssid?: string,
+    password?: string,
+    key_type?: number,
+    cypher_type?: number,
+    err_code?: number
+}
+
+interface INetRoot
+{
+    get_scaninfo?: IGetScanInfo,
+    set_stainfo?: ISetSTAInfo
+}
+//#endregion
+
+//#region System
+interface IGetSYSInfo
+{
+    relay_state?: number,
+    dev_name?: string,
+}
+
+interface ISetRelayState
+{
+    state: 0 | 1
+}
+
+interface ILedState
+{
+    off: 0 | 1
+}
+
+interface ISetDevAlias
+{
+    alias: string
+}
+
+interface ISystemNew
+{
+    get_sysinfo?: IGetSYSInfo,
+    set_relay_state?: ISetRelayState,
+    set_led_off?: ILedState,
+    set_dev_alias?: ISetDevAlias
+}
+
+interface ISystemOld
+{
+    set_dev_alias?: ISetDevAlias,
+    reboot?:
+    {
+        delay: number
+    }
+}
+
+interface ISchedule
+{
+    get_daystat?: IGetDayStat,
+    get_rules?: object
+}
+
+interface IGetDayStat
+{
+    month: number,
+    year: number,
+}
+
+interface ILightingService
+{
+    transition_light_state?: any,
+    get_light_details?: object,
+}
+
+interface IPowerOptions
+{
+    hue?: number,
+    saturation?: number,
+    brightness?: number,
+    color_temp?: number
+}
+//#endregion
+
+//#region Cloud
+interface ICloud
+{
+    get_info: object,
+}
+//#endregion
+
+//#region Formats
+interface INewFormat
+{
+    netif?: INetRoot,
+    system?: ISystemNew
+}
+
+interface IOldFormat
+{
+    [OldFormat_APName]?: INetRoot,
+    [OldFormat_LightingServiceName]?: ILightingService
+    [OldFormat_SystemName]?: ISystemOld,
+    [OldFormat_ScheduleName]?: ISchedule,
+    [OldFormat_CloudName]?: ICloud
+}
+//#endregion
+
+//#region Responses
+interface IInfoResponse
+{
+    sw_ver: string;
+    hw_ver: string;
+    model: string;
+    description: string;
+    alias: string;
+    mic_type: string;
+    dev_state: string;
+    mic_mac: string;
+    deviceId: string;
+    oemId: string;
+    hwId: string;
+    is_factory: boolean;
+    disco_ver: string;
+    ctrl_protocols: ICtrlprotocols;
+    light_state: ILightstate;
+    is_dimmable: number;
+    is_color: number;
+    is_variable_color_temp: number;
+    preferred_state: IPreferredstate[];
+    rssi: number;
+    active_mode: string;
+    heapsize: number;
+    err_code: number;
+}
+
+interface IPreferredstate
+{
+    index: number;
+    hue: number;
+    saturation: number;
+    color_temp: number;
+    brightness: number;
+}
+
+interface ILightstate
+{
+    on_off: number;
+    mode: string;
+    hue: number;
+    saturation: number;
+    color_temp: number;
+    brightness: number;
+}
+
+interface ICtrlprotocols
+{
+    name: string;
+    version: string;
+}
+//#endregion
+//#endregion
